f3187662dca501b8e406cb0ce4fb6c7902b0616c
Nate Hankes <Nate.Hankes@hydrofarm.com>
2023-10-23 21:04:46 -0700

    Astrohaus article

---
+++ posts/2023/astrohaus-freewrite-shortcuts.js
@@ -0,0 +1,47 @@
+/* INSERT MARKDOWN POST AS STRING HERE */
+export const post = `
+#Astrohaus Freewrite Keyboard Shortcuts
+
+The Astrohaus Freewrite typewriter is a distraction-free writing tool. These keyboard shortcuts will make your writing practice even more efficient.
+
+<figure>
+	<img src="https://getfreewrite.com/cdn/shop/files/SmartTypewriter-13_5000x.jpg?v=1683734481"  alt="Astrohaus Freewrite" width="400" height="400">
+	<figcaption>Photo by <a href="https://getfreewrite.com/">Astrohaus</a></figcaption>
+</figure>
+
+
+**Start a new draft**: <button style="background: red; color:white; border-radius:5px; outline:none">new</button> + <button style="background: red; color:white; border-radius:5px; outline:none">new</button>
+
+**Delete word**: <button style="background: red; color:white; border-radius:5px; outline:none">new</button> + <button style="background: white; color:black; border-radius:5px; outline:none">backspace</button>
+
+**Undo Previous**: <button style="background: red; color:white; border-radius:5px; outline:none">new</button> + <button style="background: white; color:black; border-radius:5px; outline:none">z</button>
+
+**Redo Previous**: <button style="background: red; color:white; border-radius:5px; outline:none">new</button> + <button style="background: white; color:black; border-radius:5px; outline:none">y</button>
+
+**Cycle through recent documents in a folder**: <button style="background: red; color:white; border-radius:5px; outline:none">new</button> + press <button style="background: white; color:black; border-radius:5px; outline:none">pg up</button> or <button style="background: white; color:black; border-radius:5px; outline:none">pg dn</button>
+
+<div class="mx-5 mb-4 pl-2" style="border-left: 2px solid red;>
+	Must press <button style="background: white; color:black; border-radius:5px; outline:none">pg dn</button> twice initially to go to the most recent document. Press <button style="background: white; color:black; border-radius:5px; outline:none">pg dn</button> while holding <button style="background: red; color:white; border-radius:5px; outline:none">new</button> to access older drafts and <button style="background: white; color:black; border-radius:5px; outline:none">pg up</button> to return to more recent drafts.
+</div>
+
+
+
+**Cycle Console Screen**: <button style="background: white; color:black; border-radius:5px; outline:none">special</button>
+
+**Headsup Display**: <button style="background: white; color:black; border-radius:5px; outline:none">space</button> (hold for 3 seconds)
+
+**Toggle Backlight Control**: <button style="background: white; color:black; border-radius:5px; outline:none">special</button> + <button style="background: white; color:black; border-radius:5px; outline:none">1</button> or <button style="background: white; color:black; border-radius:5px; outline:none">special</button> + <button style="background: white; color:black; border-radius:5px; outline:none">L</button>.
+
+**Device Menu**: Hold the power button for 4 seconds. 
+
+**Send draft to email**: <button style="background: white; color:black; border-radius:5px; outline:none">send</button>
+
+<div class="mx-5 mb-4 pl-2" style="border-left: 2px solid red;">
+Email address associated with your Postbox account.
+</div>
+
+**Wake and Sleep**: Short press power button
+
+**Shut Down**: Power Button (hold 15 seconds)
+
+`
