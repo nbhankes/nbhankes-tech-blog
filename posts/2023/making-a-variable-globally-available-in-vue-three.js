4f8bfbc072446d8122ee5b8c0d59484390398acc
Nate Hankes <Nate.Hankes@hydrofarm.com>
2023-11-07 11:33:13 -0800

    test

--- posts/2023/making-a-variable-globally-available-in-vue-three.js
+++ posts/2023/making-a-variable-globally-available-in-vue-three.js
@@ -66,4 +66,6 @@ v-model="dropdownSelectionRef"
 \`\`\`
 
 Now our dropdown displays all the strings stored in our array myGlobalArray.
+
+[test](https://dev-next-hydrofarm-canada-v1.azurewebsites.net/mugwumps?foo=bar&bar=foo)
 `
